2023cd929cbfeb531c96886658b03fb10400c53a
Cédric Biamonti <cedricbiamonti@MacBook-Pro-de-Cedric-2.local>
2026-04-29 09:29:29 -0400

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,22 +5,20 @@ namespace App\Providers;
 use App\Models\User;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\URL; // ✅ AJOUT
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
+        // ✅ FORCE HTTPS
+        URL::forceScheme('https');
+
         View::composer('layouts.app', function ($view) {
             $layoutCurrentUser = session('current_user_id')
                 ? User::with('club')->find(session('current_user_id'))
